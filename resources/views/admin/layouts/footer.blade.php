37ff3f3ce6369909f1f836b9395e572f11d80c67
blackonder <mehmetonderpeker@gmail.com>
2021-09-02 14:44:48 +0300

    Added table structures, migration, some admin side template

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 2.3.8
+    </div>
+    <strong>Copyright &copy; 2021 <a href="http://teknometot.com">TeknoMetot Blog</a>.</strong> Tüm hakları saklıdır.
+  </footer>
